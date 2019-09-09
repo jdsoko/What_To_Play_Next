53b8b0f1119362b012add124d26bb5b44e4380b3
John Sokolowski <johndsoko@gmail.com>
2019-09-09 17:56:39 -0500

    mobile stylings

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 'use strict'
-///fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=fallout 4&key=AIzaSyBOy_1MABfgfN5f4fcpj88B8ktpq1TY3e4`)
-///youtube  url https://www.youtube.com/watch?v=
 
+
+///Global Game Title Search Variable
+let titleOfGame = "";
+///Global Variable for Number of Search Results
+let numberOfResults = 0;
+
+let shouldScroll = true;
 
 ///Event Listener
 function formSubmit(){
-    $('form').on('submit', function(event){
+    $(document).on('submit', function(event){
         event.preventDefault();
         let gameTitle = parseTitle($('.gameInput').val());
         let resultNum = $('.numberInput').val()
         interpretTitle(gameTitle, resultNum);
-        
+        titleOfGame = gameTitle;
+        numberOfResults = resultNum; 
+        shouldScroll = true;
     })
 }
 
@@ -38,7 +45,7 @@ function interpretTitle(input1, input2){
         
     })
     .then(response => response.json())
-    .then(responseJson => getInfo(responseJson.slug, input2));
+    .then(responseJson => getInfo(responseJson.slug, input2)); 
 }
 
 
@@ -47,29 +54,58 @@ function getInfo(input1, input2){
     fetch(` https://api.rawg.io/api/games/${input1}/suggested?page_size=${input2}`)
      .then(response => response.json())
      .then(responseJson => generateResults(responseJson));
+     
+     
  }
 
 ///Adds the results from the search to the DOM
  function generateResults(input){
      console.log(input);
+     $('.landingContent').empty();
     $('.results').empty();
+    $('.landingContent').html(`<div class="landingHeader" id="top"><h1 class="smaller">What to Play Next?<i class="fas fa-gamepad" id="gamepadSmall"></i></h1></div>
+            
+    <form id="gameSearchSmall">
+        
+        <input type="text" placeholder="Previous Game Played" required class="gameInputSmall gameInput" >
+        <label class="numLabel"></label>
+        <input type="number" class="numberInputSmall numberInput" value="5" required max="25" class="resultNum" min="1" >
+        <button type="submit" class="searchButtonSmall" style="border-radius: 4px; height: 20px;"><i class="fa fa-search" id="searchIconSmall"></i></button>
+    </form>
+</div>`)
     for(let i = 0; i < input.results.length; i++){
         let genres = "";
-    for(let a = 0; a < input.results[i].genres.length; a++){
-        genres = genres.concat(`  ${input.results[i].genres[a].name}  |`)
+    if(input.results[i].genres.length > 0){
+        for(let a = 0; a < input.results[i].genres.length; a++){
+            genres = genres.concat(`  ${input.results[i].genres[a].name}  |`)
+    }}
+    else{
+        genres = "(No genres availaible)"
     }
-        $('.results').append(`<div><img src="${input.results[i].background_image}">
+        $('.results').append(`<div class="searchResult"><div id="${input.results.length}" ><img class="resultImg" src="${input.results[i].background_image}">
         <p class="gameName" >${input.results[i].name}</p>
-        <hr> <p>${genres}</p></div>
-        <button type="submit" class="moreInfo" id="${input.results[i].slug}">More Info</button>`)
+         <p class="genres">${genres}</p></div>
+        <button type="submit" class="moreInfo" id="${input.results[i].slug}">More Info</button>
+        </div>`)
     }
+        $('.results').append(`<button class="moreResults">More Results</button>`)
     $('#gameSearch').trigger('reset');
+        $('.results').append(`<div class="space"></div><div class="space"></div>`)
+    if (shouldScroll === true){
+        document.getElementById("top").scrollIntoView();;
+    }
+
+    
+    
  }
+
+ 
 
  ///More Info Button Clicked
  function moreInfo(){
     $('.results').on('click', '.moreInfo', function(event){
         event.preventDefault();
+        let originalSearchNum = $('.results div').attr('id');
         let gameSearch = $(this).attr('id');
         fetch(`https://api.rawg.io/api/games/${gameSearch}`)
         .then(response => response.json())
@@ -81,39 +117,76 @@ function getInfo(input1, input2){
 
 
 ///Generate Results from 'More Info'
- function generateMoreInfo(input1, input2 ){
+ function generateMoreInfo(input1, input2){
     console.log(input1);
     $('.results').empty();
+   
      
-     $('.results').html(`<h1>${readInfo(input1.name)}</h1>
+     $('.results').html(`<button class="backButton">Back to Results</button>
+     <h1 class="gameInfoName">${readInfo(input1.name)}</h1>
      <div class="gameMoreInfo">
     <img src="${readImage(input1.background_image)}" class="infoPic">
-    <p class="description">${readInfo(input1.description_raw)}</p>
-    <hr>
+    <h2 class="descriptionHead">Description:</h2>
+    <p class="description">${readDescription(input1.description_raw)}</p>
+    
    <ul class="gameInfo">
-    <li>Rating: ${readESRB(input1.esrb_rating)}</li>
-    <li>Meta Score: ${readInfo(input1.metacritic)}</li>
-    <li>Release Date: ${readInfo(input1.released)}</li>
+    <li class="infoListItem">Rating: <span class="listAPI">${readESRB(input1.esrb_rating)}</span></li>
+     ${metaScore(input1.metacritic)}
+    <li class="infoListItem">Release Date: <span class="listAPI">${readInfo(input1.released)}</span></li>
    </ul>
    </div>
    <div class="storeLinks">
-   <ul><span class="storeList">Where to buy: </span>${generateStoreLinks(input1).join(' ')}
+   <ul><span class="storeList">Purchase: </span><br>${generateStoreLinks(input1).join(' ')}
    </div>
    </ul>
-<iframe class="gameVid" width="420" height="315"
+<iframe class="gameVid" 
 src=${input2}>
 </iframe>
 <br>
 <form class="moreLikeGames">
-<label>Want to Find More Games like this One?</label>
-<input type="number" value="1" max="25" id="numberInput">
+<label class="likeGameLabel">Want to Find More Games like this One?</label>
+<br>
+<input class="moreGameNum" type="number" value="5" max="25" id="numberInput" required min="1" >
 <button class="moreGameButton" id="${input1.slug}">More Games</button>
 
 </form>`);
+$('.results').append(`<div class="space"></div><div class="space"></div>`)
+document.getElementById("top").scrollIntoView();;
 
  }
 
- ///Reads API response
+
+
+ ///Genrates Metacritc score result
+ function metaScore(input){
+    let score = input;
+    if (score === null) {
+        return '<li  class="infoListItem">Meta Score: <span class="listAPI">N/A</span></li>'}
+    else if (score <= 49){
+        return `<li class="infoListItem">Meta Score: <span class="meta"  style="background-color: rgb(255,0,0); color: rgb(255,255,255);">${score}</span></li>`
+    }
+    else if (score >= 50 && score <= 74){
+        return `<li class="infoListItem">Meta Score: <span class="meta" style="background-color: rgb(255,204,51); color: rgb(255,255,255);">${score}</span></li>`
+    }
+    else if(score > 75){
+        return `<li class="infoListItem">Meta Score: <span class="meta" style="background-color: rgb(102,204,51); color: rgb(255,255,255);">${score}</span></li>`
+    }
+    else {
+        console.log(score);
+    }
+ }
+
+///API Description
+function readDescription(input){
+    if(input === ''){
+        return '***Sorry, but there is no description currently available for this game.***'
+    }
+    else{
+        return input
+    }
+}
+
+ ///Reads API response for rating
  function readESRB(input){
      if(input === null){
          return `N/A`
@@ -138,25 +211,25 @@ src=${input2}>
     let storeLinks = [];
     for(let i = 0; i < input.stores.length; i++){
         if (input.stores[i].store.slug === 'steam'){
-            storeLinks.push(`<li class="link"><a href=${input.stores[i].url} target="_blank"><i class='fab fa-steam' style='font-size:36px; color: rgb(39,64,85);'></i></a></li>`);
+            storeLinks.push(`<li class="link"><a href=${input.stores[i].url} target="_blank"><i class='fab fa-steam' style='color: rgb(39,64,85);'></i></a></li>`);
         }
         else if (input.stores[i].store.slug === 'xbox360'){
-            storeLinks.push(`<li class="link"><a href=${input.stores[i].url} target="_blank"><i class='fab fa-xbox' style='font-size:36px; color: rgb(132,132,132); background-color: rgb(161,205,65);'></i></a></li>`);
+            storeLinks.push(`<li class="link"><a href=${input.stores[i].url} target="_blank"><i class='fab fa-xbox' style='color: rgb(132,132,132); background-color: rgb(161,205,65);'></i></a></li>`);
         }
         else if (input.stores[i].store.slug === 'google-play'){
-            storeLinks.push(`<li class="link"><a href=${input.stores[i].url} target="_blank"><i class='fab fa-google-play' style='font-size:36px; color: rgb(34,175,194);'></i></a></li>`);
+            storeLinks.push(`<li class="link"><a href=${input.stores[i].url} target="_blank"><i class="storeIcon" class='fab fa-google-play' style='color: rgb(34,175,194);'></i></a></li>`);
         }
         else if (input.stores[i].store.slug === 'playstation-store'){
-            storeLinks.push(`<li class="link"><a href=${input.stores[i].url} target="_blank"><i class='fab fa-playstation' style='font-size:36px; color:rgb(255,255,255); background-color: rgb(1,62,151);'></i></a></li>`);
+            storeLinks.push(`<li class="link"><a href=${input.stores[i].url} target="_blank"><i class='fab fa-playstation' style='color:rgb(255,255,255); background-color: rgb(1,62,151);'></i></a></li>`);
          }
         else if (input.stores[i].store.slug === 'nintendo'){
-            storeLinks.push(`<li class="link"><a href=${input.stores[i].url} target="_blank" ><div style="background-color: red; height:40px; width:80px; display:inline-block; padding-top: 10px;"><span style="font-size: 16px; color: white; font-family: sans-serif; width: 80px; border: 2px solid white; border-radius: 60px; padding:5px;">nintendo</span></div></a></li>`);
+            storeLinks.push(`<li class="link"><a href=${input.stores[i].url} target="_blank" ><div class="nintendo1"><span class="nintendo2">Nintendo</span></div></a></li>`);
         }
         else if (input.stores[i].store.slug === 'apple-appstore'){
-            storeLinks.push(`<li class="link"><a href=${input.stores[i].url} target="_blank"><i class='fab fa-apple' style='font-size:38px; color: rgb(68,68,68);'></i></a></li>`);
+            storeLinks.push(`<li class="link"><a href=${input.stores[i].url} target="_blank"><i class='fab fa-apple' style='color: rgb(68,68,68);'></i></a></li>`);
         }
         else if (input.stores[i].store.slug === 'xbox-store'){
-            storeLinks.push(`<li class="link"><a href=${input.stores[i].url} target="_blank"><i class='fab fa-xbox' style='font-size:36px; color: rgb(255,255,255); background-color: rgb(16,121,16)'></i></a></li>`);
+            storeLinks.push(`<li class="link"><a href=${input.stores[i].url} target="_blank"><i class='fab fa-xbox' style='color: rgb(255,255,255); background-color: rgb(16,121,16)'></i></a></li>`);
         }
         else{
             console.log('no store');
@@ -165,6 +238,16 @@ src=${input2}>
         
  }
  return storeLinks;}
+
+ ///Back Button Event Listener
+ function backButton(){
+    $('.results').on('click', '.backButton',function(event){
+        event.preventDefault();
+        shouldScroll = true;
+        interpretTitle(titleOfGame, numberOfResults);
+
+    })
+ }
 
  ///Reads API response
  function readImage(input){
@@ -179,7 +262,7 @@ src=${input2}>
  ///Youtube GET request
  function getTrailer(input1, input2){
     console.log(input2); 
-    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${input2}%20trailer&topicId=%2Fm%2F0bzvm2&type=video&key=AIzaSyBOy_1MABfgfN5f4fcpj88B8ktpq1TY3e4`)
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${input2.split(' ').join('%20')}%20original%20game%20trailer&topicId=%2Fm%2F0bzvm2&type=video&key=AIzaSyBOy_1MABfgfN5f4fcpj88B8ktpq1TY3e4`)
      .then(response => response.json())
      .then(responseJson => generateTrailerLink(responseJson, input1));
  }
@@ -192,20 +275,34 @@ src=${input2}>
     generateMoreInfo(input2, link);
  }
 
- ///More Games Button
+ ///More Games Button event listener
  function moreLikeGames(){
      $('.results').on('click', `.moreGameButton`, function(event){
         event.preventDefault();
-        let gameSearch = $('.moreGameButton').attr('id');
-        let number = document.getElementById('numberInput').value;
-        getInfo(gameSearch, number);
+        shouldScroll = true;
+        titleOfGame = $('.moreGameButton').attr('id');
+        numberOfResults = document.getElementById('numberInput').value;
+        interpretTitle(titleOfGame, numberOfResults);
      })
  }
+
+ ///More Results Button event listener
+  function moreResults(){
+      $('.results').on('click', '.moreResults', function(event){
+          event.preventDefault();
+          shouldScroll = false;
+          numberOfResults = parseInt(numberOfResults) + 5;
+          interpretTitle(titleOfGame, numberOfResults);
+      })
+  }
 
 function runApp(){
     formSubmit();
     moreInfo();
     moreLikeGames();
+    backButton();
+    moreResults();
 }
 
 $(runApp);
+
